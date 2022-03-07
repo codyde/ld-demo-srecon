d28c96623c05aa7c44a4d13061dd8b60b0bdbfb8
Cody De Arkland <codydearkland@gmail.com>
2022-03-06 21:41:53 -0800

    updating sizing of images

--- src/App.js
+++ src/App.js
@@ -11,20 +11,22 @@ function App() {
   const { qrcode, upperimage, logoversion } = useFlags();
 
   return (
-<div className={`App h-screen bg-ldls grid grid-cols-3 grid-rows-3  bg-cover bg-no-repeat`}>
-      <div className="body grid col-span-3 row-start-1 place-items-center">
+    <div className="App h-screen bg-ldls grid grid-cols-3 grid-rows-3  bg-cover bg-no-repeat">
+      {upperimage ? 
+      <div className="body grid col-span-3 row-start-1 ;xl:row-start-2 place-items-center">
         {logoversion ? (
-          <img src={toggle} className="mx-auto h-56 lg:h-80" alt="logo" />  
-        ) : (  
-            <div className="bg-white">
-            <img src={srecon} className="mx-auto h-56 lg:h-80" alt="logo" />
-            </div>
+          <img src={toggle} className="mx-auto h-56 lg:h-80" alt="logo" />
+        ) : (
+          <div className="">
+            <img src={ldlogo} className="App-pulse mx-auto h-56 lg:h-80" alt="logo" />
+          </div>
         )}
-        </div>
-        <header className="App-header grid row-start-2 col-span-3">
-        <Header />      
+      </div>
+      : <div />}
+      <header className="App-header grid row-start-2 xl:row-start-2 col-span-3">
+        <Header />
       </header>
-        <div className="body grid row-start-3 col-span-3 place-items-center">
+      <div className="body grid row-start-3 col-span-3 place-items-center">
         {qrcode ? <QRCode /> : <Login />}
       </div>
     </div>
