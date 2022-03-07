cbed0701d5d9b72c7b03fe3fd129bf39cd327290
Cody De Arkland <codydearkland@gmail.com>
2022-03-06 22:18:50 -0800

    updating sizing of images

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div className="App h-screen bg-ldls grid grid-cols-3 grid-rows-3  bg-cover bg-no-repeat">
       {upperimage ? 
-      <div className="body grid col-span-3 row-start-1 ;xl:row-start-2 place-items-center">
+      <div className="body grid col-span-3 row-start-2 xl:row-start-1 place-items-center">
         {logoversion ? (
           <img src={toggle} className="mx-auto h-56 lg:h-80" alt="logo" />
         ) : (
@@ -23,7 +23,7 @@ function App() {
         )}
       </div>
       : <div />}
-      <header className="App-header grid row-start-2 xl:row-start-2 col-span-3">
+      <header className="App-header grid row-start-1 xl:row-start-2 col-span-3">
         <Header />
       </header>
       <div className="body grid row-start-3 col-span-3 place-items-center">
