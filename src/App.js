540b8f14ef4788386141bb797087850af9ef2200
Cody De Arkland <codydearkland@gmail.com>
2022-03-06 21:20:45 -0800

    updating sizing of images

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ function App() {
 <div className={`App h-screen bg-ldls grid grid-cols-3 grid-rows-3  bg-cover bg-no-repeat`}>
       <div className="body grid col-span-3 row-start-1 place-items-center">
         {logoversion ? (
-          <img src={toggle} className="mx-auto h-28 lg:h-80" alt="logo" />  
+          <img src={toggle} className="mx-auto h-56 lg:h-80" alt="logo" />  
         ) : (  
             <div className="bg-white">
-            <img src={srecon} className="mx-auto h-28 lg:h-80" alt="logo" />
+            <img src={srecon} className="mx-auto h-56 lg:h-80" alt="logo" />
             </div>
         )}
         </div>
--- src/components/header.js
+++ src/components/header.js
@@ -3,12 +3,12 @@ import ld from '../ld-white-wide.png'
 
 export default function Header() {
   return (
-    <div className='grid h-full w-full bg-ldgray shadow-2xl place-items-center'>
+    <div className='grid h-3/4 lg:h-full w-full bg-ldgray shadow-2xl place-items-center'>
     <div className="mx-auto w-full p-4 lg:p-4 px-7 lg:px-28">
       <div>
       <img src={ld} className="mx-auto max-h-30" alt="logo" />
       </div>
-      <div className="text-xl pb-4 lg:text-6xl">
+      <div className="text-2xl pb-4 lg:text-6xl">
         <h1>Welcome to <span className="text-transparent bg-clip-text bg-gradient-to-br from-ldyellow to-lddblue">SREcon</span>!</h1>
       </div>      
     </div>
